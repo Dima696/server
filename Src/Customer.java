4c7f2cd438ec908fbd9fb88c03a84170deff0753
Dima696 <100032055+Dima696@users.noreply.github.com>
2022-05-31 19:40:56 +0300

    Update Customer.java

--- Src/Customer.java
+++ Src/Customer.java
@@ -11,28 +11,22 @@ public class Customer {
     public static void main ( String[] args ) throws NullPointerException, IOException {
         try {
 
-            var clientSocket = new Socket("localhost", 31);
+            try (var clientSocket = new Socket("localhost", 31); var reader = new BufferedReader(new InputStreamReader(System.in));
+                 var in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); var out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
 
-            var reader = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("Server access allowed!" + "\n" + "Enter your name?");
+                String word = reader.readLine();
+                out.write(word + "\n");
+                out.flush();
+                String serverWord = in.readLine();
+                System.out.println(serverWord);
+            }
+//                in.close();
+//                out.close();
 
-            System.out.println("Server access allowed!" + "\n" + "Enter your name?");
-
-            var in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            var out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-
-            String word = reader.readLine();
-            out.write(word + "\n");
-            out.flush();
-            String serverWord = in.readLine();
-            System.out.println(serverWord);
-
-        } finally {
-            clientSocket.close();
-            in.close();
-            out.close();
-
+        } catch (NullPointerException e) {
+            System.out.println();
         }
     }
 }
-
 
